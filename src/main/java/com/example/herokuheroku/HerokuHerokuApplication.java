b81d690b37d948a6e9b3dcd447da1640d5746d06
Aynalemfan <aynalemfan@gmail.com>
2022-07-25 14:20:22 -0500

    First Commit

---
+++ src/main/java/com/example/herokuheroku/HerokuHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.example.herokuheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuHerokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/herokuheroku/HerokuHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herokuheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
